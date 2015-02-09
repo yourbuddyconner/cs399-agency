5e6b500dc058cdf268a504f27227dd49255a07bb
Conner Swann <me@connerswann.me>
2015-02-09 13:32:25 -0700

    Playing around with a design, not really sure if I like it...

--- app/settings.py
+++ app/settings.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../../cs399-agency.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../cs399-agency.sqlite3'),
     }
 }
 
